a1f6d80baf00011d4716e3a4180a4038624b20cf
Thanh Tam <thanhtam.v2t@gmail.com>
2021-06-01 15:36:32 +0700

    up

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import * as constants from './src/constants'
+import * as utils from './src/common/utils'
 import WalletHandler from './src/Wallet'
 import GasStation from './src/EtherGasStation'
 
-export * as Utils from './src/common/utils'
-export * as Constants from './src/constants'
 export const Wallet = WalletHandler
 export const EtherGasStation = GasStation
+export const Constants = constants
+export const Utils = utils
